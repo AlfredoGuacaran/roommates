46e58fe09155c72c5d8c52534d34338d91f18788
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2022-02-21 23:03:15 -0300

    rutas get /roommates y /gastos -- ruta post /roommate, listas

---
+++ newRoommate.js
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import axios from 'axios';
+
+async function newRoommate() {
+  try {
+    const ramdomRoommateData = await (
+      await axios.get('https://randomuser.me/api')
+    ).data.results[0].name;
+
+    const newRoommate = {
+      nombre: ramdomRoommateData.first + ' ' + ramdomRoommateData.last,
+      id: uuidv4(),
+      debe: 0,
+      recibe: 0,
+    };
+
+    const dataBase = JSON.parse(fs.readFileSync('./roommates.json', 'utf-8'));
+    dataBase.roommates.push(newRoommate);
+
+    fs.writeFileSync('roommates.json', JSON.stringify(dataBase));
+
+    return dataBase;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+}
+
+export default newRoommate;
--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 import express from 'express';
 const app = express();
 import fs from 'fs';
+import newRoommate from './newRoommate.js';
 app.use(express.static('static'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/roommates', async (req, res) => {
+  let roommatesJSON = await JSON.parse(
+    fs.readFileSync('./roommates.json', 'utf-8')
+  );
+  let roommates = roommatesJSON.roommates;
+  // console.log(roommatesJSON);
+  res.json({ roommates });
+});
+
+app.get('/gastos', async (req, res) => {
+  let gastosJSON = await JSON.parse(fs.readFileSync('./gastos.json', 'utf-8'));
+  let gastos = gastosJSON.gastos;
+  console.log(gastos);
+  res.json({ gastos });
+});
+
+app.post('/gasto', async (req, res) => {});
+
+app.post('/roommate', async (req, res) => {
+  try {
+    const newDataBase = await newRoommate();
+    res.end();
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.listen(3000, () => console.log('Servidor corriendo en puerto 30000'));
